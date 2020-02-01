f77a284e5d1b9bf1be4645b2bdb85ca97f9614ca
Chief Nelson <chief@Chiefs-MBP.localdomain>
2020-01-31 22:32:30 -0500

    Jumping works more smoothly
    Still trying to make jumping while moving work

--- Red Planet/Assets/Scripts/PlayerMovement.cs
+++ Red Planet/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float jumpLength;
+    [SerializeField] private float jumpCooldown;
+
+    private bool jumping;
+    private float timeInAir;
+    private float timeSinceLand;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +27,42 @@ public class PlayerMovement : MonoBehaviour
         float vertInput = Input.GetAxis("Vertical");
 
         //calculate move direction
-        Vector2 movement = new Vector2(horzInput + transform.position.x, (vertInput > 0? vertInput : 0) + transform.position.y);
+        Vector2 movement = new Vector2(horzInput + transform.position.x, jumpModifier(vertInput) + transform.position.y);
 
         //actually move the player
         transform.position = Vector2.MoveTowards(transform.position, movement, speed);
+
+    }
+
+
+    private float jumpModifier(float vertInput)
+    {
+        float yMove = 0;
+
+        if (jumping)
+        {
+            timeSinceLand = 0;
+
+            //apply jump energy
+            yMove = jumpSpeed;
+
+            //check if still jumping
+            jumping = timeInAir < jumpLength;
+
+            //increment
+            timeInAir += Time.deltaTime;
+        }
+        else
+        {
+            timeInAir = 0;
+
+            //check if jumping
+            jumping = vertInput > 0 && timeSinceLand > jumpCooldown;
+
+            //increment
+            timeSinceLand += Time.deltaTime;
+        }
+
+        return yMove;
     }
 }
